a5554d08d5d44c8a748e77582a95087e553b060c
rong5026 <rong5026@naver.com>
2024-10-28 14:48:45 +0900

    :sparkles: Global Response 적용

---
+++ src/main/java/com/pricewagon/pricewagon/global/response/GlobalResponse.java
@@ -0,0 +1,14 @@
+package com.pricewagon.pricewagon.global.response;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.ErrorResponse;
+
+public record GlobalResponse(boolean success, int status, Object data, LocalDateTime time) {
+	public static GlobalResponse success(int status, Object data) {
+		return new GlobalResponse(true, status, data, LocalDateTime.now().withNano(0));
+	}
+	public static GlobalResponse fail(int stauts, ErrorResponse errorResponse) {
+		return new GlobalResponse(false, stauts, errorResponse, LocalDateTime.now().withNano(0));
+	}
+}
