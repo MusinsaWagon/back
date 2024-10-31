e6679a923b949633dd14d3da3559a1a4741dede4
Kim So Yeon <qws1566@naver.com>
2024-10-31 15:56:25 +0900

    :sparkles: feat: 회원가입 구현

--- src/main/java/com/pricewagon/pricewagon/domain/user/entity/User.java
+++ src/main/java/com/pricewagon/pricewagon/domain/user/entity/User.java
@@ -3,29 +3,23 @@ package com.pricewagon.pricewagon.domain.user.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 
 import com.pricewagon.pricewagon.domain.alarm.entity.Alarm;
 import com.pricewagon.pricewagon.domain.common.UserAuditEntity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Getter
+@Setter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User extends UserAuditEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
 	@Comment("아이디")
 	@Column(columnDefinition = "varchar(20)", nullable = false, unique = true)
@@ -34,6 +28,13 @@ public class User extends UserAuditEntity {
 	@Comment("비밀번호")
 	@Column(columnDefinition = "varchar(20)", nullable = false)
 	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private UserRole role;
+
+	public void encodedPassword(String password){
+		this.password = password;
+	}
 
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Alarm> alarms = new ArrayList<>();
--- src/main/java/com/pricewagon/pricewagon/domain/user/service/UserCommandService.java
+++ src/main/java/com/pricewagon/pricewagon/domain/user/service/UserCommandService.java
@@ -1,4 +1,14 @@
 package com.pricewagon.pricewagon.domain.user.service;
 
+import com.pricewagon.pricewagon.domain.user.dto.UserRequestDTO;
+import com.pricewagon.pricewagon.domain.user.dto.UserResponseDTO;
+import com.pricewagon.pricewagon.domain.user.entity.User;
+import com.pricewagon.pricewagon.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
 public interface UserCommandService {
+    User joinUser (UserRequestDTO.JoinDto request);
+
 }
