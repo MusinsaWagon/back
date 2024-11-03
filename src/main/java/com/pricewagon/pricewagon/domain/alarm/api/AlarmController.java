d85296461041e2672cc86a4e3828d9193e06a5fa
rong5026 <rong5026@naver.com>
2024-11-03 15:23:30 +0900

    :fire: 사용하지 않는 알람 코드 제거

--- src/main/java/com/pricewagon/pricewagon/domain/alarm/api/AlarmController.java
+++ src/main/java/com/pricewagon/pricewagon/domain/alarm/api/AlarmController.java
@@ -3,8 +3,6 @@ package com.pricewagon.pricewagon.domain.alarm.api;
 import com.pricewagon.pricewagon.domain.alarm.service.AlarmService.AlarmService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "4. [알람]", description = "알람 API")
@@ -14,6 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlarmController {
     private final AlarmService alarmService;
 
-    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE )
-    public
 }
