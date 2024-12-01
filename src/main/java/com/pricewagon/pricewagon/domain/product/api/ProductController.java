7d5dfd4f372410e4dafcae3fc3f1967cc2c01fa6
rong5026 <rong5026@naver.com>
2024-12-01 17:06:34 +0900

    :bug: import 오류난 부분 수정

--- src/main/java/com/pricewagon/pricewagon/domain/product/api/ProductController.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/api/ProductController.java
@@ -3,6 +3,7 @@ package com.pricewagon.pricewagon.domain.product.api;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
