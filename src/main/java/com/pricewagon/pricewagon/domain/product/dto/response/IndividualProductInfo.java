2ca65511e0e81852b5c9f921f774939fdbf030c6
Kim So Yeon <qws1566@naver.com>
2025-01-04 12:19:40 +0900

    :sparkles: 차트 데이터 출력 값 변경

--- src/main/java/com/pricewagon/pricewagon/domain/product/dto/response/IndividualProductInfo.java
+++ src/main/java/com/pricewagon/pricewagon/domain/product/dto/response/IndividualProductInfo.java
@@ -1,9 +1,6 @@
 package com.pricewagon.pricewagon.domain.product.dto.response;
 
-import java.util.List;
-
 import com.pricewagon.pricewagon.domain.category.dto.response.ParentAndChildCategoryDTO;
-import com.pricewagon.pricewagon.domain.history.entity.ProductHistory;
 import com.pricewagon.pricewagon.domain.product.dto.ProductDetailDto;
 import com.pricewagon.pricewagon.domain.product.entity.Product;
 
@@ -11,14 +8,21 @@ public record IndividualProductInfo(
 
 	BasicProductInfo basicProductInfo, // 메인 정보
 	ProductDetailDto productDetail, // 부과 정보
-	List<ProductHistory> productHistoryList, // 가격 히스토리
+	// List<ProductHistory> productHistoryList, // 가격 히스토리
+	ProductHistoryLists productHistoryLists,
 	ParentAndChildCategoryDTO parentAndChildCategoryDTO // 카테고리
 ) {
 
 	public static IndividualProductInfo from(Product product, BasicProductInfo basicProductInfo,
 		ParentAndChildCategoryDTO parentAndChildCategoryDTO) {
 		ProductDetailDto productDetail = ProductDetailDto.toDTO(product.getProductDetail());
-		return new IndividualProductInfo(basicProductInfo, productDetail, product.getProductHistories(),
-			parentAndChildCategoryDTO);
+		ProductHistoryLists productHistoryLists = ProductHistoryLists.createFrom(product.getProductHistories());
+
+		return new IndividualProductInfo(
+			basicProductInfo,
+			productDetail,
+			productHistoryLists,
+			parentAndChildCategoryDTO
+		);
 	}
 }
